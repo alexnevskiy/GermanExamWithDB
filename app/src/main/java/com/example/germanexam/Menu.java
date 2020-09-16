9da1bb205b902fc99caae7cfc3afe33b6930e619
Alexandr Kobyzhev <kam-2000@mail.ru>
2020-09-16 15:21:39 +0300

    Сделал кнопки в меню, переход к выбору вариантов

--- app/src/main/java/com/example/germanexam/Menu.java
+++ app/src/main/java/com/example/germanexam/Menu.java
@@ -1,6 +1,9 @@
 package com.example.germanexam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        Button buttonVariants = (Button) findViewById(R.id.button_variants);
+
+        buttonVariants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Menu.this, Variants.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+
+                }
+            }
+        });
     }
 }
