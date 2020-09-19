9e15bfcc69d9e29411525338747a3249b60ff3a5
Alex_Nevskiy <kam-2000@mail.ru>
2020-09-19 22:58:15 +0300

    Сделан начальный экран варианта, а также первое задание

---
+++ app/src/main/java/com/example/germanexam/TaskOne.java
@@ -0,0 +1,30 @@
+package com.example.germanexam;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaskOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task1);
+//        Button buttonVariants = (Button) findViewById(R.id.button_start_test);
+//
+//        buttonVariants.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    Intent intent = new Intent(TaskOne.this, Variants.class);
+//                    startActivity(intent);
+//                } catch (Exception e) {
+//
+//                }
+//            }
+//        });
+    }
+}
--- app/src/main/java/com/example/germanexam/VariantStartPage.java
+++ app/src/main/java/com/example/germanexam/VariantStartPage.java
@@ -7,19 +7,19 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Variants extends AppCompatActivity {
+public class VariantStartPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.variants);
-        Button buttonVariants = (Button) findViewById(R.id.button1);
+        setContentView(R.layout.variant_one_start);
+        Button buttonVariants = (Button) findViewById(R.id.button_start_test);
 
         buttonVariants.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    Intent intent = new Intent(Variants.this, VariantStartPage.class);
+                    Intent intent = new Intent(VariantStartPage.this, TaskOne.class);
                     startActivity(intent);
                 } catch (Exception e) {
 
