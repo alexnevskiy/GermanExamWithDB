d8a81838d7d626a8d7883b5bcb835be16c97db41
Alex_Nevskiy <kam-2000@mail.ru>
2020-10-04 20:05:24 +0300

    Добавлено окно настроек, а также нижнее навигационное меню для изменения данных

--- app/src/main/java/com/example/germanexam/Menu.java
+++ app/src/main/java/com/example/germanexam/Menu.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Menu extends AppCompatActivity {
 
@@ -13,6 +18,9 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
         Button buttonVariants = (Button) findViewById(R.id.button_variants);
 
         buttonVariants.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +28,34 @@ public class Menu extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     Intent intent = new Intent(Menu.this, Variants.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        Button buttonExam = (Button) findViewById(R.id.button_exam);
+
+        buttonExam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Menu.this, VariantStartPage.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        Button buttonSettings = (Button) findViewById(R.id.button_settings);
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Menu.this, Settings.class);
                     startActivity(intent);
                 } catch (Exception e) {
 
