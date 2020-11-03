865e152ebba3654c078d24e5f5965fc745a4be56
Alex_Nevskiy <kam-2000@mail.ru>
2020-11-03 19:00:30 +0300

    Реализовано 1 задание 5 лабы + картинка

---
+++ labs/lab5/SimpleUITest/app/src/androidTest/java/com/example/simpleuitest/EspressoTest.java
@@ -0,0 +1,59 @@
+package com.example.simpleuitest;
+
+import android.content.pm.ActivityInfo;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void rotateFromPortraitToLandscape() {
+        activityRule.getScenario().onActivity(activity ->
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT));
+        onView(withId(R.id.button)).check(matches(withText("Tap on me")));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).check(matches(withText("1 tap")));
+        onView(withId(R.id.editText)).perform(clearText());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).check(matches(withText("2 tap")));
+        onView(withId(R.id.editText)).perform(typeText("Espresso Test"));
+        activityRule.getScenario().onActivity(activity ->
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE));
+        onView(withId(R.id.button)).check(matches(withText("Tap on me")));
+        onView(withId(R.id.editText)).check(matches(withText("Espresso Test")));
+    }
+
+    @Test
+    public void rotateFromLandscapeToPortrait() {
+        activityRule.getScenario().onActivity(activity ->
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE));
+        onView(withId(R.id.button)).check(matches(withText("Tap on me")));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).check(matches(withText("1 tap")));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.button)).check(matches(withText("2 tap")));
+        onView(withId(R.id.editText)).perform(clearText());
+        onView(withId(R.id.editText)).perform(typeText("Espresso Test"));
+        activityRule.getScenario().onActivity(activity ->
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT));
+        onView(withId(R.id.button)).check(matches(withText("Tap on me")));
+        onView(withId(R.id.editText)).check(matches(withText("Espresso Test")));
+    }
+}
---
+++ labs/lab5/SimpleUITest/app/src/main/java/com/example/simpleuitest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.simpleuitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                button.setText(counter + " tap");
+            }
+        });
+    }
+}
